a6cc35f732d765afeaa1a09b9de110897d7e5edf
Andrea Ribuoli <andrea@powerruby.com>
2018-11-23 11:40:50 +1345

    fix application.rb, clever_utility_job.rb

--- v01/app/jobs/clever_utility_job.rb
+++ v01/app/jobs/clever_utility_job.rb
@@ -1,7 +1,25 @@
 class CleverUtilityJob < ApplicationJob
   queue_as :default
 
+  after_enqueue do |job|
+    ActionCable.server.broadcast 'web_events_channel', job_id: job.job_id, request: arguments[0], status: 'waiting'
+  end
+
+  after_perform do |job|
+    ActionCable.server.broadcast 'web_events_channel', job_id: job.job_id, status: @status, jobret: @msg
+  end
+
   def perform(*args)
-    # Do something later
+    logger.info "Executing Job with arguments: #{args}"
+    l = args[0].size
+    c = ActiveRecord::Base.connection
+    escaped_cmd = args[0].gsub("'", "''")
+    @status = 'completed'
+    begin
+      c.execute("CALL QCMDEXC('#{escaped_cmd}', #{l})")
+    rescue Exception => e
+      @status = 'failed'
+      @msg = "#{e}"
+    end
   end
 end
